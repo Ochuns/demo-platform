a261863bc0cbe3b15812c4762f88801ac9979738
Ochun <sekura.shunei@gmail.com>
2025-11-12 15:40:29 +0900

    登録画面の更新

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const Page = () => {
-  return (
-    <div>P</div>
-  )
+    return (
+        <div>
+            <Label htmlFor="location">場所の名前</Label>
+            <Input id="location"/>
+            <Label htmlFor="map">GoogleマップのURL</Label>
+            <Input id="map"/>
+            <Label htmlFor="reason">おすすめ理由</Label>
+            <Input id="reason"/>
+            <Label htmlFor="money">予算</Label>
+            <Input id="money"/>
+        </div>
+    )
 }
 
 export default Page
