3aa37189e5f19d9ad8ec40c7fa44681d25904af2
Ochun <sekura.shunei@gmail.com>
2025-11-12 16:34:06 +0900

    マップリンクの追加

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,13 +1,43 @@
 import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
-const Page = () => {
-  const supabase = createClient();
-  const {data} = supabase.from("places").select("*");
+const Page = async () => {
 
-
-  return (
-    <div>page</div>
-  )
+    const supabase = await createClient();
+    const { data, error } = await supabase.from("places").select("*");
+    if (error) {
+        console.log("error", error)
+    }
+    console.log("data", data)
+    return (
+        <div>
+            {data?.map((place) => (
+                <Card key={place.id}>
+                    <CardHeader>
+                        <CardTitle>{place.location}</CardTitle>
+                        <CardDescription>{place.reason}</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>{place.details}</p>
+                    </CardContent>
+                    <CardFooter>
+                        <Link href={`${place.map}`} target="_blank" className="text-blue-600 hover:underline">
+                            場所を見る
+                        </Link>
+                    </CardFooter>
+                </Card>
+            ))}
+        </div>
+    )
 }
 
 export default Page
