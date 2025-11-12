198acd705b864e6e96eee6036d0a7d940f70fc3e
Ochun <sekura.shunei@gmail.com>
2025-11-12 15:59:13 +0900

    登録画面のボタンとsupabase

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 const Page = () => {
+    const [location, setLocation] = useState("")
+    const [map, setMap] = useState("")
+    const [reason, setReason] = useState("")
+    const [money, setMoney] = useState<number>(0)
+
     return (
         <div>
             <Label htmlFor="location">場所の名前</Label>
-            <Input id="location"/>
+            <Input id="location" value={location} onChange={(e) => setLocation(e.target.value)}/>
             <Label htmlFor="map">GoogleマップのURL</Label>
-            <Input id="map"/>
+            <Input id="map" value={map} onChange={(e) => setMap(e.target.value)}/>
             <Label htmlFor="reason">おすすめ理由</Label>
-            <Input id="reason"/>
+            <Input id="reason" value={reason} onChange={(e) => setReason(e.target.value)}/>
             <Label htmlFor="money">予算</Label>
-            <Input id="money"/>
+            <Input id="money" value={money} onChange={(e) => setMoney(Number(e.target.value))}/>
+            <Button variant="outline">追加</Button>
         </div>
     )
 }
